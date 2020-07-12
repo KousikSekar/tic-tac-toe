2db5b5bdc8ccab62b634660289fae7a204edf4e5
Kousik <kousik.sekar@gmail.com>
2020-07-12 19:49:21 +0530

    added home button

--- howtoplay.py
+++ howtoplay.py
@@ -20,7 +20,7 @@ next_path = r"D:\learning\Python\tic-tac-toe\right.png"
 rule1_path = r"D:\learning\Python\tic-tac-toe\horizontal.PNG"
 rule2_path = r"D:\learning\Python\tic-tac-toe\vertical.png"
 rule3_path = r"D:\learning\Python\tic-tac-toe\diagonal.PNG"
-
+gotit_path = r"D:\learning\Python\tic-tac-toe\button_got-it.png"
 
 
 prev_img = ImageTk.PhotoImage(Image.open(prev_path))
@@ -28,6 +28,7 @@ next_img = ImageTk.PhotoImage(Image.open(next_path))
 rule1_img = ImageTk.PhotoImage(Image.open(rule1_path))
 rule2_img = ImageTk.PhotoImage(Image.open(rule2_path))
 rule3_img = ImageTk.PhotoImage(Image.open(rule3_path))
+gotit_img = ImageTk.PhotoImage(Image.open(gotit_path))
 rules = [rule1_img,rule2_img,rule3_img]
 
 
@@ -44,6 +45,7 @@ def rule_change(event,cmnd):
             rule = rule + 1
             labl = tk.Label(window,image=rules[rule])
     labl.grid(row=1,column=2)
+
     
 
 
@@ -55,13 +57,19 @@ prev_btn.bind('<Button-1>',lambda event : rule_change(event,'prev'))
 next_btn = tk.Button(window,anchor='e',image=next_img,bd=0,bg='white')
 next_btn.bind('<Button-1>',lambda event : rule_change(event,'next'))
 
+gotit_btn = tk.Button(window,image=gotit_img,bd=0,bg='white')
+
 labl = tk.Label(window,image=rules[rule])
     
-
+space = tk.Button(window,text='      ',bd=0)
+space.grid(row=2,column=0)
 ins.grid(row=0,column=2,pady=20)
-prev_btn.grid(row=1,column=1,padx=10)
+prev_btn.grid(row=1,column=1,padx=5)
 next_btn.grid(row=1,column=3,padx=5)
-labl.grid(row=1,column=2,padx=5)
+labl.grid(row=1,column=2,padx=5,pady=10)
+gotit_btn.grid(row=2,column=2)
+
+
 
 
 window.mainloop()
