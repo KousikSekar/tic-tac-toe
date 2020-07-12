1658bdbad0336d04859feee1fb61b2665247389c
Kousik <kousik.sekar@gmail.com>
2020-07-12 21:21:41 +0530

    added spin box

--- player_select.py
+++ player_select.py
@@ -12,12 +12,18 @@ window.resizable(0,0)
 head = tk.Label(window,text='VS',font=("Arial",50),bg='white')
 player_btn = tk.Button(window,text='Player',font=("Arial ",50),bg='white',fg='Black',width='10',bd=10)
 comp_btn = tk.Button(window,text='Computer',font=("Arial ",50),bg='Black',fg='white',width='10',bd=10)
+spin = tk.Spinbox(window,values=(1,2,3,4,5,6,7,8,9,10),width=5)
+space = tk.Label(window,text='*********',fg='white',bg='white')
+matches = tk.Label(window,text='Number of matches (1 to 10) :',font=("Arial",15),bg='white')
 
-head.pack()
-player_btn.pack()
-comp_btn.pack()
+space.grid(row=0,column=0)
+head.grid(row=0,column=1,columnspan=2)
+player_btn.grid(row=1,column=1,columnspan=2)
+comp_btn.grid(row=2,column=1,columnspan=2)
+matches.grid(row=3,column=1)
+spin.grid(row=3,column=2,pady=10)
 
-
+# print(spin.get())
 
 
 window.mainloop()
